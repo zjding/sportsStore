e840482a3b566d351c0a228b5f1254f1c7fb997d
zjding <zjding@outlook.com>
2014-09-15 15:54:16 -0500

    First version

---
+++ SportsStore.UI/angularjs/controllers/sportsStore.js
@@ -0,0 +1,34 @@
+﻿angular.module("sportsStore")
+    .constant("dataUrl", "http://localhost:2403/products")
+    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+    //$scope.data = {
+    //    products: [
+    //        {
+    //            name: "Product #1", description: "A product",
+    //            category: "Category #1", price: 100
+    //        },
+    //        {
+    //            name: "Product #2", description: "A product",
+    //            category: "Category #1", price: 110
+    //        },
+    //        {
+    //            name: "Product #3", description: "A product",
+    //            category: "Category #2", price: 210
+    //        },
+    //        {
+    //            name: "Product #4", description: "A product",
+    //            category: "Category #3", price: 202
+    //        }
+    //    ]
+    //};
+
+    $scope.data = {};
+
+    $http.get(dataUrl)
+        .success(function(data) {
+            $scope.data.products = data;
+        })
+        .error(function(error) {
+            $scope.data.error = error;
+        });
+});
